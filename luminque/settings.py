dc3b79e45b40ea599d7c69a5d705eb32003000ca
Ksenia Sossin <sossin.ksenia@gmail.com>
2024-02-12 21:12:59 +0000

    add to home app templates

--- luminque/settings.py
+++ luminque/settings.py
@@ -63,7 +63,10 @@ ROOT_URLCONF = 'luminque.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [TEMPLATES_DIR],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'templates', 'allauth')
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
