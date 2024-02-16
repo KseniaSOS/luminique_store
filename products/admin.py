0e09ad29a694b8198f8f35410a649ea3a250898c
Ksenia Sossin <sossin.ksenia@gmail.com>
2024-02-16 12:38:06 +0000

    Customized admin

--- products/admin.py
+++ products/admin.py
@@ -1,5 +1,30 @@
 from django.contrib import admin
 from .models import Product, Category
 
-admin.site.register(Product)
-admin.site.register(Category)
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    """  
+    This class adds fields from the Recipe model to the admin
+    panel
+    """
+
+    list_display = (
+        'sku',
+        'date',
+        'name',
+        'category',
+        'price',        
+        'image',
+    )
+
+    search_fields = ['name', 'category']
+
+    ordering = ('sku',)
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (        
+        'name',        
+    )
