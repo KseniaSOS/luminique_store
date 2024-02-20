ddc9202097fa3792dada2bd4b6d3786f55bcf7a0
Ksenia Sossin <sossin.ksenia@gmail.com>
2024-02-20 13:29:36 +0000

    Add bag context and delivery logic

--- luminque/settings.py
+++ luminque/settings.py
@@ -76,6 +76,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'bag.contexts.bag_contents',
             ],
         },
     },
@@ -164,3 +165,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+FREE_DELIVERY_THRESHOLD = 49
+STANDARD_DELIVERY_PERCENTAGE = 10
