e25b0a9900c7d788b6ba0758edba52fe0719b009
Ksenia Sossin <sossin.ksenia@gmail.com>
2024-02-27 14:10:13 +0000

    Add forms, admin and signals

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,3 +1,40 @@
 from django.contrib import admin
+from .models import Order, OrderLineItem
 
-# Register your models here.
+
+class OrderLineItemAdminInline(admin.TabularInline):
+    """
+    This class creates inline item, which will allow to add,
+    edit line items in the admin area.
+    """
+
+    model = OrderLineItem
+    readonly_fields = ('lineitem_total',)
+
+
+class OrderAdmin(admin.ModelAdmin):
+    """
+    This class adds functionality to
+    OrderLinItemAdminInline in the admin area.
+    """
+
+    inlines = (OrderLineItemAdminInline,)
+
+    readonly_fields = ('order_number', 'date',
+                       'delivery_cost', 'order_total',
+                       'grand_total',)
+
+    fields = ('order_number', 'date', 'full_name', 'email',
+              'phone_number', 'country', 'postcode',
+              'town_or_city', 'street_address1',
+              'street_address2', 'county', 'delivery_cost',
+              'order_total', 'grand_total',)
+
+    list_display = ('order_number', 'date', 'full_name',
+                    'order_total', 'delivery_cost',
+                    'grand_total',)
+
+    ordering = ('-date',)
+
+
+admin.site.register(Order, OrderAdmin)
