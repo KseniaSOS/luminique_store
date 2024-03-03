aac8c0ff944001d4f2271e354d0abf5b168641c6
Ksenia Sossin <sossin.ksenia@gmail.com>
2024-03-03 09:50:56 +0000

    Add class methods to webhook handler and webhook view

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -21,8 +21,7 @@ class StripeWH_Handler:
     
     def handle_payment_intent_succeeded(self, event):
         """
-        Handle the payment_intent.payment_succeeded
-        webhook from Stripe
+        Handle the payment_intent.succeeded webhook from Stripe
         """
 
         return HttpResponse(
--- checkout/webhooks.py
+++ checkout/webhooks.py
@@ -27,8 +27,8 @@ def webhook(request):
 
     try:
         event = stripe.Webhook.construct_event(
-            payload, sig_header, wh_secret
-            )
+        payload, sig_header, wh_secret
+        )
     except ValueError as e:
         # Invalid payload
         return HttpResponse(status=400)
@@ -38,3 +38,22 @@ def webhook(request):
     except Exception as e:
         return HttpResponse(content=e, status=400)
 
+    # Set up a webhook handler
+    handler = StripeWH_Handler(request)
+
+    # Map webhook events to relevant handler functions
+    event_map = {
+        'payment_intent.succeeded': handler.handle_payment_intent_succeeded,
+        'payment_intent.payment_failed': handler.handle_payment_intent_payment_failed,
+    }
+
+    # Get the webhook type from Stripe
+    event_type = event['type']
+
+    # If there's a handler for it, get it from the event map
+    # Use the generic one by default
+    event_handler = event_map.get(event_type, handler.handle_event)
+
+    # Call the event handler with the event
+    response = event_handler(event)
+    return response
