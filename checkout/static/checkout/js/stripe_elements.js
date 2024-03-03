036e952bfef7a2b5b2383a549beeb0d0b1d00ba1
Ksenia Sossin <sossin.ksenia@gmail.com>
2024-03-03 09:34:16 +0000

    Add webhooks and webhook handler

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -53,7 +53,7 @@ form.addEventListener('submit', function(ev) {
     ev.preventDefault();
     card.update({ 'disabled': true});
     $('#submit-button').attr('disabled', true);
-    $('#payments-form').fadeToggle(100);
+    $('#payment-form').fadeToggle(100);
     $('#loading-overlay').fadeToggle(100);
     stripe.confirmCardPayment(clientSecret, {
         payment_method: {
@@ -68,7 +68,7 @@ form.addEventListener('submit', function(ev) {
                 </span>
                 <span>${result.error.message}</span>`;
             $(errorDiv).html(html);
-            $('#payments-form').fadeToggle(100);
+            $('#payment-form').fadeToggle(100);
             $('#loading-overlay').fadeToggle(100);
             card.update({ 'disabled': false});
             $('#submit-button').attr('disabled', false);
