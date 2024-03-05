2de7d499260ea30ca5e05f7e17d82f32cf087589
Ksenia Sossin <sossin.ksenia@gmail.com>
2024-03-05 14:27:56 +0000

    Admin Area - add deleting products functionality

--- products/views.py
+++ products/views.py
@@ -79,11 +79,12 @@ def add_product(request):
     if request.method == 'POST':
         form = ProductForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()
+            product = form.save()
             messages.success(request, 'Successfully added product!')
-            return redirect(reverse('add_product'))
+            return redirect(reverse('product_detail', args=[product.id]))
         else:
-            messages.error(request, 'Failed to add product. Please ensure the form is valid.')
+            messages.error(request, 'Failed to add product. '
+            'Please ensure the form is valid.')
     else:
         form = ProductForm()
         
@@ -130,3 +131,12 @@ def edit_product(request, product_id):
     }
 
     return render(request, template, context)
+
+
+@login_required
+def delete_product(request, product_id):
+    """ Delete a product from the store """
+    product = get_object_or_404(Product, pk=product_id)
+    product.delete()
+    messages.success(request, 'Product deleted!')
+    return redirect(reverse('products'))
