f8f84bd17c58ca08ae3193cd137bbee8f33657bb
Ksenia Sossin <sossin.ksenia@gmail.com>
2024-03-05 10:33:20 +0000

    Add order history and update checkout view

--- profiles/views.py
+++ profiles/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
 from .models import UserProfile
 from .forms import UserProfileForm
 
+from checkout.models import Order
+
+@login_required
 def profile(request):
     """ Display the user's profile. """
 
@@ -27,3 +31,25 @@ def profile(request):
     }
 
     return render(request, template, context)
+
+
+@login_required
+def order_history(request, order_number):
+    """This view displays the order history
+    on a checkout success page"""
+
+    order = get_object_or_404(Order, order_number=order_number)
+
+    messages.info(request, (
+            f'This is a past confirmation for order number {order_number}. '
+            'A confirmation email was sent on the order date.'
+    ))
+
+    template = 'checkout/checkout_success.html'
+    context = {
+        'order': order,        
+    }
+
+    return render(request, template, context)
+
+
