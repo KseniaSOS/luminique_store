2a5317fa65dfa0d1e6d9c9fd90c5a37d87e493ea
Ksenia Sossin <sossin.ksenia@gmail.com>
2024-03-06 12:15:06 +0000

    Add blog app and models

---
+++ blog/models.py
@@ -0,0 +1,37 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+STATUS = ((0, 'Draft'), (1, 'Published'))
+
+
+class BlogCategory(models.Model):
+    """
+    This class creates the Blog categories.
+    """
+    name = models.CharField(max_length=200)
+    
+    class Meta:        
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+
+
+class Blog(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    featured_image = models.ImageField(null=True, blank=True)
+    content = models.TextField()
+    excerpt = models.TextField(blank=True)
+    tag = models.ForeignKey('BlogCategory', null=True, blank=True, 
+                            on_delete=models.SET_NULL)
+    author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL,
+                               related_name="blog_posts")
+    status = models.IntegerField(choices=STATUS, default=0)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return self.name
--- luminque/settings.py
+++ luminque/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
     'bag',
     'checkout',
     'profiles',
+    'blog',
     'crispy_forms',
     'crispy_bootstrap5',
 ]
