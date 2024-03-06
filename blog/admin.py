5523c310cb107d143f4eaba54b18edc0138ab9b3
Ksenia Sossin <sossin.ksenia@gmail.com>
2024-03-06 12:35:30 +0000

    Customized Admin area for the blog content

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,38 @@
 from django.contrib import admin
 from .models import Blog, BlogCategory
 
-admin.site.register(Blog)
-admin.site.register(BlogCategory)
+
+@admin.register(BlogCategory)
+class BlogCategoryAdmin(admin.ModelAdmin):
+    """
+    Adds fields to the admin area 
+    """
+
+    list_display = ('name', )
+    list_filter = ('name', )
+
+
+
+@admin.register(Blog)
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'excerpt', 'tag', 'created_on',
+                    'author', 'featured_image', 'status',)
+    search_fields = ['title', 'tag']
+    prepopulated_fields = {'slug': ('title',)}
+    list_filter = ('status', 'created_on')
+    actions = ['publish', 'unpublish']
+
+    def publish(self, request, queryset):
+        """
+        This help method updates the status field to 1
+        """
+
+        queryset.update(status=1)
+
+    def unpublish(self, request, queryset):
+        """
+        This help method updates the status field to 0
+        """
+
+        queryset.update(status=0)
+
--- blog/models.py
+++ blog/models.py
@@ -10,7 +10,8 @@ class BlogCategory(models.Model):
     """
     name = models.CharField(max_length=200)
     
-    class Meta:        
+    class Meta:
+        verbose_name_plural = 'Blog Categories'   
         ordering = ['name']
 
     def __str__(self):
