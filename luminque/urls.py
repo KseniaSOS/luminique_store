1b39a751a405807aef262afaf8ea4a34f99076fa
Ksenia Sossin <sossin.ksenia@gmail.com>
2024-03-09 19:17:13 +0000

    set up 404.html, set Debug to False for a trial deployment

--- luminque/urls.py
+++ luminque/urls.py
@@ -28,3 +28,4 @@ urlpatterns = [
     path('profile/', include('profiles.urls')),
     path('blog/', include('blog.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+handler404 = "sweetcode.views.page_not_found"
