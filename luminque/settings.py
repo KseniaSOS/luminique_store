7b83048004dc9af1e91966d07092ebffc30b87dc
Ksenia Sossin <sossin.ksenia@gmail.com>
2024-03-10 15:59:17 +0000

    set debug

--- luminque/settings.py
+++ luminque/settings.py
@@ -27,7 +27,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['luminique-3d156ce4ed79.herokuapp.com', '8000-kseniasos-luminiquestor-7vug2wc8s1r.ws-eu108.gitpod.io']
 
@@ -135,9 +135,18 @@ WSGI_APPLICATION = 'luminque.wsgi.application'
 #     }
 # }
 
-DATABASES = {
-    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
-}
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
