491e07acd64672944a2f52d5393bc2d4756c5684
Ksenia Sossin <sossin.ksenia@gmail.com>
2024-03-11 12:20:22 +0000

    add new code l ine in settings.py to collect staticfiles, bug: aws still cant upload static parent folder, just child folders

--- luminque/settings.py
+++ luminque/settings.py
@@ -27,8 +27,8 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = 'DEVELOPMENT' in os.environ
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
+# DEBUG = True
 
 ALLOWED_HOSTS = ['luminique-3d156ce4ed79.herokuapp.com', '8000-kseniasos-luminiquestor-7vug2wc8s1r.ws-eu108.gitpod.io']
 
@@ -186,6 +186,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 MEDIA_URL = '/media/'
