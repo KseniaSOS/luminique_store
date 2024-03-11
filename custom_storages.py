336d04671cf6ef0b032e83373b47940ef83be3b9
Ksenia Sossin <sossin.ksenia@gmail.com>
2024-03-11 13:32:39 +0000

    Add changes to custom_storages.py file

--- custom_storages.py
+++ custom_storages.py
@@ -3,8 +3,8 @@ from storages.backends.s3boto3 import S3Boto3Storage
 
 
 class StaticStorage(S3Boto3Storage):
-    locations = settings.STATICFILES_LOCATION
+    location = settings.STATICFILES_LOCATION
 
 
 class MediaStorage(S3Boto3Storage):
-    locations = settings.MEDIAFILES_LOCATION
+    location = settings.MEDIAFILES_LOCATION
